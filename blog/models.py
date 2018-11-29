e3681c4d1eb47b128ecbc32f3f32142dc25ca7a4
eliashhtores <33284999+eliashhtores@users.noreply.github.com>
2018-11-28 21:10:51 -0600

    Added home page and improvements

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
